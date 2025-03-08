c1c5de461470ed5bc215e4a725b6cf888750a368
dhrkcoll <dharak.collossus@mail.ru>
2025-03-08 12:07:49 +0500

    change product item

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,83 @@
 import React from "react";
 import styles from "./Cart.module.scss";
 import { useSelector } from "react-redux";
-
+import image from "../../../public/860.png";
+const products = [
+  {
+    id: 1,
+    title: "Burger",
+    price: 200,
+    description: "Its much more tasty burger",
+    image_path: image,
+  },
+  {
+    id: 2,
+    title: "Burger",
+    price: 200,
+    description: "Its much more tasty burger",
+    image_path: image,
+  },
+  {
+    id: 3,
+    title: "Burger",
+    price: 200,
+    description: "Its much more tasty burger",
+    image_path: image,
+  },
+  {
+    id: 4,
+    title: "Burger",
+    price: 200,
+    description: "Its much more tasty burger",
+    image_path: image,
+  },
+  {
+    id: 5,
+    title: "Burger",
+    price: 200,
+    description: "Its much more tasty burger",
+    image_path: image,
+  },
+  {
+    id: 6,
+    title: "Burger",
+    price: 200,
+    description: "Its much more tasty burger",
+    image_path: image,
+  },
+];
 const Cart = () => {
-  const cartItems = useSelector((state) => state.cart);
-  return <div className={styles.cart}></div>;
+  const cartItems = useSelector((state) => state.cart.items);
+  console.log(cartItems);
+  return (
+    <div className={styles.cart}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Твои заказы</h1>
+        <button className={styles.button}>Изменить</button>
+      </div>
+
+      <ul>
+        {products.map((item) => {
+          return (
+            <li key={item.id} className={styles.item}>
+              <div className={styles.itemInner}>
+                <div className={styles.image}>
+                  <img src={"../../../public/860.jpg"} alt="" />
+                </div>
+
+                <div>
+                  <h2 className={styles.itemTitle}>{item.title}</h2>
+                  <p className={styles.description}>{item.description}</p>
+                </div>
+              </div>
+
+              <div className={styles.price}>{item.price} руб.</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Cart;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import ProductList from "../components/ProductList/ProductList.jsx";
 import Form from "../components/Form/Form.jsx";
+import Cart from "../components/Cart/Cart.jsx";
 
 export const router = createBrowserRouter([
   { index: "/", element: <ProductList /> },
   { path: "/form", element: <Form /> },
+  { path: "/cart", element: <Cart /> },
 ]);
