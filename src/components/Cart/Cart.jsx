2ed327e5b3472af3c2e825d6043659c77303500a
dhrkcoll <dharak.collossus@mail.ru>
2025-03-11 10:03:08 +0500

    Added product detail page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Cart.module.scss";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartItem from "../CartItem/CartItem";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
@@ -9,27 +11,16 @@ const Cart = () => {
     <div className={styles.cart}>
       <div className={styles.header}>
         <h1 className={styles.title}>Твои заказы</h1>
-        <button className={styles.button}>Изменить</button>
+        <button className={styles.button}>
+          <Link to={"/"} style={{ color: "white" }}>
+            Изменить
+          </Link>
+        </button>
       </div>
 
       <ul>
         {cartItems.map((item) => {
-          return (
-            <li key={item.id} className={styles.item}>
-              <div className={styles.image}>
-                <img src={item.image_path} alt="" />
-              </div>
-
-              <div className={styles.label}>
-                <h2 className={styles.itemTitle}>
-                  {item.title} <span>{item?.count}</span>
-                </h2>
-                <p className={styles.description}>{item.description}</p>
-              </div>
-
-              <div className={styles.price}>{item.price}₽</div>
-            </li>
-          );
+          return <CartItem item={item} />;
         })}
       </ul>
 
---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./CartItem.module.scss";
+
+const CartItem = ({ item }) => {
+  const totalPrice = item.count * item.price;
+  return (
+    <li key={item.id} className={styles.item}>
+      <div className={styles.image}>
+        <img src={item.image_path} alt={item.title} />
+      </div>
+
+      <div className={styles.label}>
+        <h2 className={styles.itemTitle}>
+          {item.title} <span>{item?.count}Х</span>
+        </h2>
+        <p className={styles.description}>{item.description}</p>
+      </div>
+
+      <div className={styles.price}>{totalPrice}₽</div>
+    </li>
+  );
+};
+
+export default CartItem;
