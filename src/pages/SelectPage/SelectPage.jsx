f38cf5de58c6b801400c7c075881aafacd7e707d
dhrkcoll <dharak.collossus@mail.ru>
2025-03-13 12:40:28 +0500

    Added product detail page

--- src/pages/SelectPage/SelectPage.jsx
+++ src/pages/SelectPage/SelectPage.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { useEffect, useCallback } from "react";
 import styles from "./SelectPage.module.scss";
-import { Link } from "react-router-dom";
 import { FaCheck } from "react-icons/fa";
+import { useTelegram } from "../../hooks/useTelegram.js";
+import { useNavigate } from "react-router-dom";
 
 const SelectPage = () => {
+  const navigate = useNavigate();
+  const { tg } = useTelegram();
+
+  const onClickBackButton = useCallback(() => {
+    navigate(-1);
+  }, [navigate]);
+
+  useEffect(() => {
+    if (!window.Telegram || !tg) {
+      return;
+    }
+
+    const backButton = tg.BackButton;
+
+    backButton.show();
+    backButton.onClick(onClickBackButton);
+
+    return () => {
+      backButton.hide();
+      backButton.offClick(onClickBackButton);
+    };
+  }, [tg, onClickBackButton]);
+
   return (
     <div className={styles.selectPageContainer}>
       <div className={styles.catalogRestaurantsList}>
