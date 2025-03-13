d69cab67c72135901078e17becbee3e95b2e5892
dhrkcoll <dharak.collossus@mail.ru>
2025-03-13 11:04:08 +0500

    Added product detail page

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import styles from "./Menu.module.scss";
 
 const Menu = () => {
-  return <div></div>;
+  return (
+    <div className={styles.menuContainer}>
+      <div className={styles.profile}>
+        <div className={styles.profileInner}>
+          <section className={styles.sectionProfile}>
+            <div className={styles.sectionProfileInfo}>
+              <div className={styles.avatar}>
+                <img src="" alt="" />
+              </div>
+              <div className={styles.info}>
+                <div className={styles.userName}></div>
+                <div className={styles.userId}></div>
+              </div>
+            </div>
+          </section>
+          <div data-v-14aa940a="" class="section-menu">
+            <div data-v-14aa940a="" class="menu-content">
+              <div data-v-14aa940a="" class="menu-content-item">
+                <img
+                  data-v-14aa940a=""
+                  class="icons-30-orders"
+                  src="/img/icons-30-orders0.1ad6293d.svg"
+                />
+                <div data-v-14aa940a="" class="text">
+                  <div data-v-14aa940a="" class="div3">
+                    Мои заказы
+                  </div>
+                  <div data-v-14aa940a="" class="_18"></div>
+                  <div data-v-14aa940a="" class="icons-16-angle-right">
+                    <img
+                      data-v-14aa940a=""
+                      class="group-12"
+                      src="/img/group-11.9d48cbdd.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div data-v-14aa940a="" class="menu-content-item">
+                <img
+                  data-v-14aa940a=""
+                  class="icons-30-wallet"
+                  src="/img/icons-30-wallet0.0e6c8f60.svg"
+                />
+                <div data-v-14aa940a="" class="text">
+                  <div data-v-14aa940a="" class="div3">
+                    Баланс
+                  </div>
+                  <div data-v-14aa940a="" class="_890">
+                    500,00&nbsp;₽
+                  </div>
+                </div>
+              </div>
+              <div data-v-14aa940a="" class="menu-content-item">
+                <img
+                  data-v-14aa940a=""
+                  class="icons-30-map"
+                  src="/img/icons-30-map0.f4d6ba2b.svg"
+                />
+                <div data-v-14aa940a="" class="text2">
+                  <div data-v-14aa940a="" class="div3">
+                    Мои адреса
+                  </div>
+                  <div data-v-14aa940a="" class="_18"></div>
+                  <div data-v-14aa940a="" class="icons-16-angle-right">
+                    <img
+                      data-v-14aa940a=""
+                      class="group-14"
+                      src="/img/group-13.9d48cbdd.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,10 +4,12 @@ import Form from "../components/Form/Form.jsx";
 import Cart from "../components/Cart/Cart.jsx";
 import ProductPage from "../pages/ProductPage/ProductPage.jsx";
 import App from "../App.jsx";
+import Menu from "../pages/Menu/Menu.jsx";
 
 export const router = createBrowserRouter([
   { index: "/", element: <App /> },
   { path: "/form", element: <Form /> },
   { path: "/cart", element: <Cart /> },
   { path: "/products/:productId", element: <ProductPage /> },
+  { path: "/menu", element: <Menu /> },
 ]);
