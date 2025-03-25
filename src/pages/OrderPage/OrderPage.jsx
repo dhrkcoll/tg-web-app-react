304090d77d6726b7ebb910c7a519d092a51f6967
dhrkcoll <dharak.collossus@mail.ru>
2025-03-25 23:12:02 +0500

    Shared phone

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -19,22 +19,21 @@ const OrderPage = () => {
   const handleRequestPhone = () => {
     tg.requestContact((contact) => {
       if (contact && contact.phone_number) {
-        setUserPhone(contact.phone_number);
-        // const data = JSON.stringify({ phone: contact.phone_number });
-        // tg.sendData(data);
+        const data = JSON.stringify({ phone: contact.phone_number });
+        tg.sendData(data);
       }
     });
   };
 
-  // useEffect(() => {
-  //   tg.onEvent("dataReceived", (data) => {
-  //     console.log(data);
-  //     const contact = JSON.parse(data);
-  //     if (contact && contact.phone_number) {
-  //       console.log("Получен номер телефона:", contact.phone_number);
-  //     }
-  //   });
-  // }, [tg]);
+  useEffect(() => {
+    tg.onEvent("dataReceived", (data) => {
+      const contact = JSON.parse(data);
+      if (contact && contact.phone_number) {
+        setUserPhone(contact.phone_number);
+        console.log("Получен номер телефона:", contact.phone_number);
+      }
+    });
+  }, [tg]);
 
   return (
     <div className={styles.deliveryModule}>
